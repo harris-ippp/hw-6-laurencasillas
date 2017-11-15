f75ce224034f205cdf0161fa229ef7e6211b7199
Moloq <vmhvilchis@gmail.com>
2017-11-15 15:34:50 -0600

    TA comments

--- e1.py
+++ e1.py
@@ -4,7 +4,9 @@ import requests
 from bs4 import BeautifulSoup as bs
 
 resp = requests.get("http://historical.elections.virginia.gov/elections/search/year_from:1924/year_to:2016/office_id:1/stage:General")
-soup = (resp.content, "html.parser")
+### TA: You are not defining the soup correctly. You are making a tuple here, not a BeautifulSoup object
+### you needed to do soup = bs(resp....)
+soup = bs(resp.content, "html.parser")
 
 #create list with election IDs
 row = soup.find_all("tr", {"class":"election_item"}) #find the tag and look at election item in the class
@@ -20,5 +22,7 @@ year = [row.text for row in year]
 ELECTION_ID = list(zip(year, election_item))
 
 #export as txt file
-with open("ELECTION_ID.txt", "w") as f:
-f.write('\n'.join('{} {}'.format(x[0],x[1]) for x in YEARS_IDS))
+#with open("ELECTION_ID.txt", "w") as f:
+    ### TA: You needed to indent this line.
+    ### TA: Also, YEARS_IDS is not defined. what is it? where do you say what it is in the code before?
+    #f.write('\n'.join('{} {}'.format(x[0],x[1]) for x in YEARS_IDS))
